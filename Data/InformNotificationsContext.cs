26ceb40314baa43164a7261ab452b02409618dc7
AidenGariepy89 <79092752+AidenGariepy89@users.noreply.github.com>
2023-02-15 18:45:30 -0500

    Seed data works with recipients (no parent support yet)

--- Data/InformNotificationsContext.cs
+++ Data/InformNotificationsContext.cs
@@ -26,37 +26,49 @@ public class InformNotificationsContext : DbContext
             .WithMany(r => r.Parents)
             .HasForeignKey(p => p.RecipientId);
 
-        modelBuilder.Entity<Recipient>().HasData(new Recipient { RecipientId = 1 });
-        modelBuilder.Entity<Student>()
-            .HasData(new Student
-            {
-                RecipientId = 1,
-                StudentId = 1,
-                FirstName = "Aiden",
-                LastName = "Gariepy",
-                GraduationYear = "2023",
-                EmailAddress = "aidgar23@gcasda.org",
-                PhoneNumber = "7062638995"
-            });
-        modelBuilder.Entity<Parent>()
-            .HasData(new Parent
-            {
-                RecipientId = 1,
-                ParentId = 1,
-                FirstName = "Serge",
-                LastName = "Gariepy",
-                EmailAddress = "sgariepy@gcasda.org",
-                PhoneNumber = "7062177138"
-            });
-        modelBuilder.Entity<Parent>()
-            .HasData(new Parent
-            {
-                RecipientId = 1,
-                ParentId = 2,
-                FirstName = "LeAnn",
-                LastName = "Gariepy",
-                EmailAddress = "lgariepy@gcasda.org",
-                PhoneNumber = "7062177139"
-            });
+        // modelBuilder.Entity<Recipient>().HasData(new Recipient { RecipientId = 1 });
+        // modelBuilder.Entity<Student>()
+        //     .HasData(new Student
+        //     {
+        //         RecipientId = 1,
+        //         StudentId = 1,
+        //         FirstName = "Aiden",
+        //         LastName = "Gariepy",
+        //         GraduationYear = "2023",
+        //         EmailAddress = "aidgar23@gcasda.org",
+        //         PhoneNumber = "7062638995"
+        //     });
+        // modelBuilder.Entity<Parent>()
+        //     .HasData(new Parent
+        //     {
+        //         RecipientId = 1,
+        //         ParentId = 1,
+        //         FirstName = "Serge",
+        //         LastName = "Gariepy",
+        //         EmailAddress = "sgariepy@gcasda.org",
+        //         PhoneNumber = "7062177138"
+        //     });
+        // modelBuilder.Entity<Parent>()
+        //     .HasData(new Parent
+        //     {
+        //         RecipientId = 1,
+        //         ParentId = 2,
+        //         FirstName = "LeAnn",
+        //         LastName = "Gariepy",
+        //         EmailAddress = "lgariepy@gcasda.org",
+        //         PhoneNumber = "7062177139"
+        //     });
+        
+        List<Student> students = SeedData.ReadFile();
+
+        int i = 0;
+        foreach (var student in students)
+        {
+            i++;
+
+            modelBuilder.Entity<Recipient>().HasData(new Recipient { RecipientId = i });
+            student.RecipientId = i;
+            modelBuilder.Entity<Student>().HasData(student);
+        }
     }
 }
