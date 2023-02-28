39a6adf0b52afdc0889fe65dee26a164e814ca60
AidenGariepy89 <79092752+AidenGariepy89@users.noreply.github.com>
2023-02-27 22:14:45 -0500

    Added todo

--- Models/SentMessage.cs
+++ Models/SentMessage.cs
@@ -8,12 +8,3 @@ public class SentMessage
     public DateTime TimeDelivered { get; set; }
     public string Contents { get; set; } = string.Empty;
 }
-
-// public class SentMessage
-// {
-//     public int MessageId { get; set; }
-//     public DateTime TimeDelivered { get; set; }
-//     public List<int> RecipientIds { get; set; }
-//     public List<Recipient> Recipients { get; set; }
-//     public string? Contents { get; set; }
-// }
